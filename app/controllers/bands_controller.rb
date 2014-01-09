5d12356b883abec61a5a21b7b1c877c6bc04ab55
zumbalogy <intrapostal@gmail.com>
2014-01-09 18:43:32 -0500

    can make and see new band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,7 +1,20 @@
 class BandsController < ApplicationController
     def new
     end
+
+    def create
+        name = params[:name]
+        band = Band.new
+        band.name = name
+        band.save
+
+        redirect_to '/bands'
+    end
+
     
+    def index
+        @all = Band.all
+    end
 
 end
 
